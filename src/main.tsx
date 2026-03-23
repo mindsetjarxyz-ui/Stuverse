2b62d7176535dbd43fc987607605b58ba28fbed5
mindsetjarxyz-ui <mindsetjar.xyz@gmail.com>
2026-03-23 19:22:08 +0600

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -5,33 +5,19 @@ import './index.css';
 
 // Fix for "Cannot set property fetch of #<Window> which has only a getter"
 // This happens in some iframe environments where window.fetch is read-only.
-// We try multiple ways to make it writable or at least provide a setter that does nothing.
-(function patchFetch() {
-  const patch = (obj: any) => {
-    const descriptor = Object.getOwnPropertyDescriptor(obj, 'fetch');
-    if (descriptor && !descriptor.configurable) {
-      return false;
+try {
+  const originalFetch = window.fetch;
+  Object.defineProperty(window, 'fetch', {
+    configurable: true,
+    enumerable: true,
+    get: () => originalFetch,
+    set: () => {
+      // Silently ignore attempts to overwrite fetch in read-only environments
     }
-    const originalFetch = obj.fetch;
-    try {
-      Object.defineProperty(obj, 'fetch', {
-        configurable: true,
-        enumerable: true,
-        get: () => originalFetch,
-        set: () => {
-          // Silently ignore attempts to overwrite fetch
-        }
-      });
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
-
-  if (!patch(window)) {
-    patch(globalThis);
-  }
-})();
+  });
+} catch (e) {
+  // Silently ignore patching errors
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
